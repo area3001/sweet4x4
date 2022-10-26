41a4a8a702e2561176fddd09cd335455dad4ad41
Hans Polders <hans.polders@gmail.com>
2022-10-26 20:54:41 +0200

    rotary encoder

--- kmk/code.py
+++ kmk/code.py
@@ -1,13 +1,11 @@
-print("Starting")
-
 import board
 
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 from kmk.extensions.RGB import RGB
+from kmk.extensions.media_keys import MediaKeys
 
-rgb = RGB(pixel_pin=board.D6, num_pixels=9)
 
 # rotary 4+5
 
@@ -17,7 +15,21 @@ keyboard.col_pins = (board.D0,board.D1,board.D2,board.D3)
 keyboard.row_pins = (board.D7,board.D8,board.D9,board.D10)
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
+# LED's
+rgb = RGB(pixel_pin=board.D6, num_pixels=9)
 keyboard.extensions.append(rgb)
+keyboard.extensions.append(MediaKeys())
+
+# Rotary Encoder
+# https://github.com/KMKfw/kmk_firmware/blob/master/docs/en/encoder.md
+from kmk.modules.encoder import EncoderHandler
+# from kmk.modules import layers
+encoder_handler = EncoderHandler()
+keyboard.modules = [encoder_handler]
+encoder_handler.pins = (
+    (board.D4, board.D5, None,), # encoder #1 
+    )
+encoder_handler.map = [ (( KC.VOLD, KC.VOLU, None), ) ]
 
 keyboard.keymap = [
     [KC.RGB_MODE_RAINBOW,KC.B,KC.C,KC.D,
